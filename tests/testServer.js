77d820657c5b7b57e50b6fc30be8feca344ef810
vvek87 <vvek87@gmal.com>
2019-03-26 16:57:12 -0500

    add more tests for server/db

--- tests/testServer.js
+++ tests/testServer.js
@@ -1,8 +1,10 @@
 // if db is empty, run 'npm run seed' to populate it
 // make sure express server is NOT running before running server test script
 
+
 var chai = require('chai');
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 var chaiHTTP = require('chai-http');
 
 var app = require('../server/index.js');
@@ -12,7 +14,26 @@ chai.use(chaiHTTP);
 
 describe('Server Tests', () => {
 
-  it('Should GET reviews sorted by most recent date', (done) => {
+  it('Should respond with 5-100 reviews', (done) => {
+    chai.request(app)
+      .get('/totalReviews')
+      .end((err, res) => {
+        assert.isAtLeast(res.body.reviews.length, 5);
+        assert.isAtMost(res.body.reviews.length, 100);
+        done();
+      });
+  });
+
+  it('Should also respond with a criteria object containing 7 categories', (done) => {
+    chai.request(app)
+      .get('/totalReviews')
+      .end((err, res) => {
+        expect(Object.keys(res.body.criteria).length).to.equal(7);
+        done();
+      });
+  });
+
+  it('Should respond with reviews sorted by most recent date', (done) => {
     chai.request(app)
       .get('/totalReviews')
       .end((err, res) => {
@@ -23,7 +44,7 @@ describe('Server Tests', () => {
       });
   });
 
-  it('Should GET reviews with specific data format', (done) => {
+  it('Should respond with reviews in a specific data format', (done) => {
     chai.request(app)
       .get('/totalReviews')
       .end((err, res) => {
@@ -39,6 +60,44 @@ describe('Server Tests', () => {
         expect(typeof res.body.reviews[0].checkin).to.equal('number');
         expect(typeof res.body.reviews[0].value).to.equal('number');
         expect(typeof res.body.reviews[0].avgRating).to.equal('number');
+        done();
+      });
+  });
+
+  it('Should calculate the average rating of a category between all reviews', (done) => {
+    chai.request(app)
+      .get('/totalReviews')
+      .end((err, res) => {
+
+        var avgAccuracy = 0;
+
+        for (var i = 0; i < res.body.reviews.length; i++) {
+          avgAccuracy += res.body.reviews[i].accuracy;
+        }
+
+        avgAccuracy = (avgAccuracy / res.body.reviews.length).toFixed(2);
+        avgAccuracy = parseFloat(avgAccuracy);
+
+        expect(avgAccuracy).to.equal(res.body.criteria.accuracy);
+        done();
+      });
+  });
+
+  it('Should calculate the average rating between all reviews', (done) => {
+    chai.request(app)
+      .get('/totalReviews')
+      .end((err, res) => {
+
+        var avgRating = 0;
+
+        for (var i = 0; i < res.body.reviews.length; i++) {
+          avgRating += res.body.reviews[i].avgRating;
+        }
+
+        avgRating = (avgRating / res.body.reviews.length).toFixed(2);
+        avgRating = parseFloat(avgRating);
+
+        expect(avgRating).to.equal(res.body.criteria.totalRating);
         done();
       });
   });
