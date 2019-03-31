e265dcbfa0236f205c060fe59756c05adbf5382b
vvek87 <vvek87@gmal.com>
2019-03-30 19:09:31 -0500

    refactor testDB.js and testServer.js to work with Travis CI

--- tests/testDB.js
+++ tests/testDB.js
@@ -3,11 +3,6 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 
-var loremHipsum = require('lorem-hipsum');
-var dateGen = require('random-date-generator');
-var genName = require('node-random-name');
-var picUrl = 'https://i.kinja-img.com/gawker-media/image/upload/s--K-l8wGJH--/c_scale,f_auto,fl_progressive,q_80,w_800/jqncra5xcu3ajxoreokh.jpg';
-
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/testDB', { useNewUrlParser: true });
 
@@ -31,57 +26,28 @@ var testReviewSchema = new mongoose.Schema({
 
 var testReview = mongoose.model('Review', testReviewSchema);
 
-var genReviewText = () => {
-  return loremHipsum({
-    count: 3,
-    units: 'sentences',
-    sentenceLowerBound: 1,
-    sentenceUpperBound: 5
-  });
-};
-
-var genDate = () => {
-  dateGen.getRandomDate();
-  var startDate = new Date(1600, 1, 1);
-  var endDate = new Date(2023, 1, 1);
-  return dateGen.getRandomDateInRange(startDate, endDate);
-};
-
-var genNum = () => {
-  return Math.floor(Math.random() * (6 - 1) + 1);
-};
-
-
 var createReview = () => {
 
-  var name = genName({ first: true, random: Math.random });
-  var text = genReviewText();
-  var date = genDate();
-  var accuracy = genNum();
-  var communication = genNum();
-  var cleanliness = genNum();
-  var location = genNum();
-  var checkin = genNum();
-  var value = genNum();
-  var avgRating = (accuracy + communication + cleanliness + location + checkin + value) / 6;
+  for (var i = 0; i < 10; i++) {
 
-  new testReview({
-    picture: picUrl,
-    name,
-    date,
-    text,
-    accuracy,
-    communication,
-    cleanliness,
-    location,
-    checkin,
-    value,
-    avgRating: avgRating.toFixed(2)
-  }).save((err) => {
-    if (err) {
-      console.log('Create document in test db error: ', err);
-    }
-  });
+    new testReview({
+      picture: 'test url',
+      name: 'test name',
+      date: new Date('December 17, 1995 03:24:00'),
+      text: 'test text',
+      accuracy: 3,
+      communication: 3,
+      cleanliness: 3,
+      location: 3,
+      checkin: 3,
+      value: 3,
+      avgRating: 3
+    }).save((err) => {
+      if (err) {
+        console.log('Create document in test db error: ', err);
+      }
+    });
+  }
 
   console.log('test db populated');
 };
@@ -89,7 +55,7 @@ var createReview = () => {
 createReview();
 
 var revsByDate = (cb) => {
-  testReview.find({}).sort('-date').exec((err, reviews) => {
+  testReview.find({}).sort('-date').lean().exec((err, reviews) => {
     if (err) {
       console.log('sorted reviews error: ', err);
       cb(err, null);
@@ -107,7 +73,7 @@ describe('Database Tests', () => {
       assert.isAtMost(results[0].accuracy, 5);
       done();
     });
-  }).timeout(3000);
+  }).timeout(6000);
 
   it('The avgRating should be the average of the six category ratings', (done) => {
     revsByDate((err, results) => {
@@ -122,7 +88,7 @@ describe('Database Tests', () => {
       expect(results[0].avgRating).to.equal(average);
       done();
     });
-  });
+  }).timeout(3000);
 
   it('Should store reviews with specific data format', (done) => {
     revsByDate((err, results) => {
@@ -138,10 +104,11 @@ describe('Database Tests', () => {
       expect(typeof results[0].checkin).to.equal('number');
       expect(typeof results[0].value).to.equal('number');
       expect(typeof results[0].avgRating).to.equal('number');
-      mongoose.connection.db.dropDatabase();
       done();
     });
-  });
+  }).timeout(3000);
 
 });
 
+
+module.exports.testReview = testReview;
--- tests/testServer.js
+++ tests/testServer.js
@@ -1,34 +1,143 @@
 // if db is empty, run 'npm run seed' to populate it
 // make sure express server is NOT running before running server test script
 
-
 var chai = require('chai');
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 var chaiHTTP = require('chai-http');
 
 var moment = require('moment');
+var dateGen = require('random-date-generator');
 
 var app = require('../server/index.js');
 
 chai.use(chaiHTTP);
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testDB', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', () => { console.log('test db connection error'); });
+db.once('open', () => { console.log('connected to test db (testDB.js)'); });
+
+var testDBReviewSchema = new mongoose.Schema({
+  picture: String,
+  name: String,
+  date: Date,
+  text: String,
+  accuracy: Number,
+  communication: Number,
+  cleanliness: Number,
+  location: Number,
+  checkin: Number,
+  value: Number,
+  avgRating: Number,
+});
+
+var testDBReview = mongoose.model('DBReview', testDBReviewSchema);
+
+var genDate = () => {
+  dateGen.getRandomDate();
+  var startDate = new Date(1600, 1, 1);
+  var endDate = new Date(2023, 1, 1);
+  return dateGen.getRandomDateInRange(startDate, endDate);
+};
+
+var createDBReview = () => {
+
+  for (var i = 0; i < 10; i++) {
+
+    new testDBReview({
+      picture: 'test url',
+      name: 'test name',
+      date: genDate(),
+      text: 'test text',
+      accuracy: 3,
+      communication: 3,
+      cleanliness: 3,
+      location: 3,
+      checkin: 3,
+      value: 3,
+      avgRating: 3
+    }).save((err) => {
+      if (err) {
+        console.log('Create document in test db error: ', err);
+      }
+    });
+  }
+
+  console.log('test db populated');
+};
+
+createDBReview();
+
+var revsByDate = (cb) => {
+  testDBReview.find({}).sort('-date').lean().exec((err, reviews) => {
+    if (err) {
+      console.log('sorted reviews error: ', err);
+      cb(err, null);
+    }
+    cb(null, reviews);
+  });
+};
+
+
+app.get('/testTotalReviews', (req, res) => {
+
+  revsByDate((err, allReviews) => {
+
+    if (err) {
+      console.log('Get sorted reviews server error: ', err);
+      res.send(404);
+    } else {
+      var reviews = allReviews.slice(0, 11);
+      var criteria = {
+        accuracy: 0,
+        communication: 0,
+        cleanliness: 0,
+        location: 0,
+        checkin: 0,
+        value: 0,
+        totalRating: 0
+      };
+
+      for (var i = 0; i < reviews.length; i++) {
+        var formatDate = moment(reviews[i].date).format('MMMM YYYY').split(' ');
+        reviews[i].shortDate = formatDate.join(' ');
+
+        criteria.accuracy += reviews[i].accuracy;
+        criteria.communication += reviews[i].communication;
+        criteria.cleanliness += reviews[i].cleanliness;
+        criteria.location += reviews[i].location;
+        criteria.checkin += reviews[i].checkin;
+        criteria.value += reviews[i].value;
+        criteria.totalRating += reviews[i].avgRating;
+      }
+
+      for (var category in criteria) {
+        criteria[category] = (criteria[category] / 11).toFixed(2);
+        criteria[category] = parseFloat(criteria[category]);
+      }
+
+      res.send({reviews, criteria});
+    }
+  });
+});
 
 describe('Server Tests', () => {
 
-  it('Should respond with 5-100 reviews', (done) => {
+  it('Should respond with reviews', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
-        assert.isAtLeast(res.body.reviews.length, 5);
-        assert.isAtMost(res.body.reviews.length, 100);
+        assert.isAtLeast(res.body.reviews.length, 1);
         done();
       });
-  });
+  }).timeout(8000);
 
   it('Should also respond with a criteria object containing 7 categories', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
         expect(Object.keys(res.body.criteria).length).to.equal(7);
         done();
@@ -37,18 +146,16 @@ describe('Server Tests', () => {
 
   it('Should respond with reviews sorted by most recent date', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
         expect(res.body.reviews[0].date > res.body.reviews[1].date).to.equal(true);
-        expect(res.body.reviews[1].date > res.body.reviews[2].date).to.equal(true);
-        expect(res.body.reviews[3].date > res.body.reviews[4].date).to.equal(true);
         done();
       });
   });
 
   it('Should save a modified version of the date property to shortDate', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
 
         var formatDate = moment(res.body.reviews[0].date).format('MMMM YYYY').split(' ');
@@ -61,7 +168,7 @@ describe('Server Tests', () => {
 
   it('Should respond with reviews in a specific data format', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
         expect(typeof res.body.reviews[0]).to.equal('object');
         expect(typeof res.body.reviews[0].picture).to.equal('string');
@@ -82,7 +189,7 @@ describe('Server Tests', () => {
 
   it('Should calculate the average rating of a category between all reviews', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
 
         var avgAccuracy = 0;
@@ -101,7 +208,7 @@ describe('Server Tests', () => {
 
   it('Should calculate the average rating between all reviews', (done) => {
     chai.request(app)
-      .get('/totalReviews')
+      .get('/testTotalReviews')
       .end((err, res) => {
 
         var avgRating = 0;
