7b0ea258c6b978498e111259556f8654877960fc
vvek87 <vvek87@gmal.com>
2019-04-01 15:20:58 -0500

    apply css style to center content

--- client/src/components/totalReviews.jsx
+++ client/src/components/totalReviews.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 import Stars from './stars.jsx';
 
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding-top: 16px;
+  padding-bottom: 16px;
+  font-weight: 800
+  font-size: 24px
+  color: #484848
+  border-top: 1px solid #d2d2d2
+  border-bottom: 1px solid #d2d2d2
+`;
+
 
 export default class TotalReviews extends React.Component {
   constructor(props) {
@@ -11,10 +23,10 @@ export default class TotalReviews extends React.Component {
   render() {
     if (this.props.reviews.length) {
       return (
-        <div id="totalRevsContainer">
+        <Wrapper>
           <div>{this.props.reviews.length} Reviews</div>
           <div><Stars rating={this.props.criteria.totalRating}/></div>
-        </div>
+        </Wrapper>
       );
     } else {
       return (
